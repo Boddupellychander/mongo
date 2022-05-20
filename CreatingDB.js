31c9096f1ff12d699030d94c92faefdb4d3a02da
Chanderbc <103108023+Chanderbc@users.noreply.github.com>
2022-05-20 05:38:24 +0530

    Add files via upload

---
+++ CreatingDB.js
@@ -0,0 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var connectionString = "mongodb://127.0.0.1:27017/HRMS"
+var db;
+MongoClient.connect(connectionString, { useUnifiedTopology: true }, function(err, db) {
+    if (err) console.error(err);
+    console.log("DB Connected");
+    var dbobj = db.db("HRMS");
+    dbobj.createCollection("projectid", function(err, res) {
+        if (err) console.error(err);
+        console.log("Collection Connected");
+        db.close();
+    });
+
+});
